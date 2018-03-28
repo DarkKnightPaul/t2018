d690da535030e53f0fd13c3a279e966dced9a31d
huhuiyu <1069306849@qq.com>
2018-03-29 07:50:07 +0800

    js03添加了显示当前时间功能

--- java/课程/java/WebBase/WebContent/js03.js
+++ java/课程/java/WebBase/WebContent/js03.js
@@ -12,3 +12,22 @@ function reverseInterest() {
     interest[i].checked = !interest[i].checked;
   }
 }
+
+function formatNow() {
+  var date = new Date();
+  var sdate = date.getFullYear() + '年';
+  sdate += date.getMonth() + 1 + '月';
+  sdate += date.getDate() + '日 ';
+  sdate += date.getHours() + ':';
+  sdate += date.getMinutes() + ':';
+  sdate += date.getSeconds();
+  return sdate;
+}
+
+function showTime() {
+  var spTime = document.getElementById('spTime');
+  spTime.innerHTML = formatNow();
+}
+
+showTime();
+setInterval(showTime, 1000);
