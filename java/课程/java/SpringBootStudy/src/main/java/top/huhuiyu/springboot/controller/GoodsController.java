8dd259c9b4bdcf6a08a4a70d1b7c6ef15eab1d23
huhuiyu <1069306849@qq.com>
2018-05-02 13:10:07 +0800

    服务器消息添加了success，goods添加完成

--- java/课程/java/SpringBootStudy/src/main/java/top/huhuiyu/springboot/controller/GoodsController.java
+++ java/课程/java/SpringBootStudy/src/main/java/top/huhuiyu/springboot/controller/GoodsController.java
@@ -1,16 +1,20 @@
 package top.huhuiyu.springboot.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import top.huhuiyu.springboot.dao.TbGoodsDAO;
+import top.huhuiyu.springboot.entity.TbGoods;
 import top.huhuiyu.springboot.util.JsonMessage;
 
 @RestController
 @RequestMapping("/goods")
 public class GoodsController {
+  private static final Logger LOG = LoggerFactory.getLogger(GoodsController.class);
 
   @Autowired
   private TbGoodsDAO tbGoodsDAO;
@@ -23,4 +27,27 @@ public class GoodsController {
     return json;
   }
 
+  @RequestMapping("/add")
+  @ResponseBody
+  public JsonMessage add(TbGoods goods) throws Exception {
+    LOG.debug("goods:" + goods);
+    if (JsonMessage.isEmpty(goods.getGname())) {
+      return JsonMessage.getFail("商品名称必须填写！");
+    }
+    if (JsonMessage.isEmpty(goods.getGinfo())) {
+      return JsonMessage.getFail("商品信息必须填写！");
+    }
+    if (goods.getAmount() <= 0) {
+      return JsonMessage.getFail("商品数量必须大于0！");
+    }
+    if (goods.getPrice().compareTo(JsonMessage.ZERO) < 1) {
+      return JsonMessage.getFail("商品价格必须大于0！");
+    }
+    if (goods.getSellprice().compareTo(JsonMessage.ZERO) < 1) {
+      return JsonMessage.getFail("商品销售价格必须大于0！");
+    }
+    tbGoodsDAO.add(goods);
+    JsonMessage json = JsonMessage.getSuccess("添加成功");
+    return json;
+  }
 }
--- java/课程/java/SpringBootStudy/src/main/java/top/huhuiyu/springboot/util/JsonMessage.java
+++ java/课程/java/SpringBootStudy/src/main/java/top/huhuiyu/springboot/util/JsonMessage.java
@@ -1,16 +1,23 @@
 package top.huhuiyu.springboot.util;
 
 import java.io.Serializable;
+import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
 
 public class JsonMessage implements Serializable {
   private static final long serialVersionUID = 8823355291602843181L;
+  public static final BigDecimal ZERO = new BigDecimal("0");
+  private boolean success = false;
   private int code = 500;
   private String message = "服务器忙，请稍后重试";
   private Map<String, Object> datas = new HashMap<String, Object>();
 
   public JsonMessage() {
+  }
+
+  public static boolean isEmpty(String str) {
+    return (str == null) || (str.trim().equals(""));
   }
 
   public void putData(String key, Object data) {
@@ -22,18 +29,27 @@ public class JsonMessage implements Serializable {
   }
 
   public static JsonMessage getFail(String message) {
-    return getMessage(500, message);
+    return getMessage(false, 500, message);
   }
 
   public static JsonMessage getSuccess(String message) {
-    return getMessage(200, message);
+    return getMessage(true, 200, message);
   }
 
-  public static JsonMessage getMessage(int code, String message) {
+  public static JsonMessage getMessage(boolean success, int code, String message) {
     JsonMessage json = new JsonMessage();
+    json.setSuccess(success);
     json.setCode(code);
     json.setMessage(message);
     return json;
+  }
+
+  public boolean isSuccess() {
+    return success;
+  }
+
+  public void setSuccess(boolean success) {
+    this.success = success;
   }
 
   public int getCode() {
@@ -51,7 +67,7 @@ public class JsonMessage implements Serializable {
   public void setMessage(String message) {
     this.message = message;
   }
-  
+
   public Map<String, Object> getDatas() {
     return datas;
   }
