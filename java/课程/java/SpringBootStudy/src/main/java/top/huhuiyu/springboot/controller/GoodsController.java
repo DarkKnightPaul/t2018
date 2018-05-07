8f6a07e2e2b182f1c05305bac5a4bd7055a2f835
huhuiyu <1069306849@qq.com>
2018-05-07 14:51:07 +0800

    SpringBoot后台Goods功能全部完成

--- java/课程/java/SpringBootStudy/src/main/java/top/huhuiyu/springboot/controller/GoodsController.java
+++ java/课程/java/SpringBootStudy/src/main/java/top/huhuiyu/springboot/controller/GoodsController.java
@@ -50,4 +50,57 @@ public class GoodsController {
     JsonMessage json = JsonMessage.getSuccess("添加成功");
     return json;
   }
+
+  @RequestMapping("/delete")
+  @ResponseBody
+  public JsonMessage delete(TbGoods goods) throws Exception {
+    LOG.debug("goods:" + goods);
+    if (goods.getGid() <= 0) {
+      return JsonMessage.getFail("请提供商品编号！");
+    }
+    tbGoodsDAO.delete(goods);
+    JsonMessage json = JsonMessage.getSuccess("删除成功");
+    return json;
+  }
+
+  @RequestMapping("/queryByKey")
+  @ResponseBody
+  public JsonMessage queryByKey(TbGoods goods) throws Exception {
+    LOG.debug("goods:" + goods);
+    if (goods.getGid() <= 0) {
+      return JsonMessage.getFail("请提供商品编号！");
+    }
+    goods = tbGoodsDAO.queryByKey(goods);
+    JsonMessage json = JsonMessage.getSuccess("查询成功");
+    json.putData("goods", goods);
+    return json;
+  }
+
+  @RequestMapping("/modify")
+  @ResponseBody
+  public JsonMessage modify(TbGoods goods) throws Exception {
+    LOG.debug("goods:" + goods);
+    if (goods.getGid() < 1) {
+      return JsonMessage.getFail("请选择商品！");
+    }
+    if (JsonMessage.isEmpty(goods.getGname())) {
+      return JsonMessage.getFail("商品名称必须填写！");
+    }
+    if (JsonMessage.isEmpty(goods.getGinfo())) {
+      return JsonMessage.getFail("商品信息必须填写！");
+    }
+    if (goods.getAmount() <= 0) {
+      return JsonMessage.getFail("商品数量必须大于0！");
+    }
+    if (goods.getPrice().compareTo(JsonMessage.ZERO) < 1) {
+      return JsonMessage.getFail("商品价格必须大于0！");
+    }
+    if (goods.getSellprice().compareTo(JsonMessage.ZERO) < 1) {
+      return JsonMessage.getFail("商品销售价格必须大于0！");
+    }
+    tbGoodsDAO.modify(goods);
+    JsonMessage json = JsonMessage.getSuccess("修改成功");
+    return json;
+  }
+
 }
